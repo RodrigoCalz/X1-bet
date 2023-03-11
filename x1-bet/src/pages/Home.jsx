0fc932c2b9d27e9deea284402aea124048c2b543
Rodrigo Calazans Santos <rodrigoocalazans@gmail.com>
2023-03-10 21:01:59 -0300

    Header

--- x1-bet/src/pages/Home.jsx
+++ x1-bet/src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { InputEntrar, Main, NavBar } from '../styles/HomeStyle'
 
 const Home = () => {
   return (
-    <div>
-        Home
-    </div>
+    <Main>
+        <NavBar>
+          <a href="http://localhost:3000/">
+            <img src="http://cambista.x1-betse.com/usuarios/getpic/26" alt="logo" width="205px"/>
+          </a> 
+          <div>
+            <input type="text" name='login' id='login' placeholder='Login'/>
+            <input type="password" name="password" id="password" placeholder='Senha' />
+            <InputEntrar type="submit" value="Entrar" name='submit' id='submit' />
+          </div> 
+        </NavBar>
+    </Main>
   )
 }
 
---
+++ x1-bet/src/styles/HomeStyle.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Main = styled.body`
+    background-color: black;
+    height: 100vh;
+    color: white;
+    margin: 0;
+    padding: 0;
+`;
+
+export const NavBar = styled.form`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    padding: 20px;
+`;
+
+export const InputEntrar = styled.input`
+    padding: 10px;
+
+`;
